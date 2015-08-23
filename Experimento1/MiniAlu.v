71a5d609822336db1349f91f8196f0ed054df9b1
Javier <javier.brito29@gmail.com>
2015-08-22 18:51:47 -0600

    Experimento1

---
+++ Experimento1/MiniAlu.v
@@ -0,0 +1,205 @@
+
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+
+module MiniAlu
+(
+ input wire Clock,
+ input wire Reset,
+ output wire [7:0] oLed
+
+ 
+);
+
+wire [15:0]  wIP,wIP_temp;
+reg         rWriteEnable,rBranchTaken;
+wire [27:0] wInstruction;
+wire [3:0]  wOperation;
+reg [15:0]   rResult;
+wire [7:0]  wSourceAddr0,wSourceAddr1,wDestination,wDestination_LastOperation;
+wire [15:0] wSourceData0,wSourceDataRAM0,wSourceData1,wSourceDataRAM1,wIPInitialValue,wImmediateValue,wResult_Last;
+
+ROM InstructionRom 
+(
+	.iAddress(     wIP          ),
+	.oInstruction( wInstruction )
+);
+
+assign wSourceData0 = (wDestination_LastOperation == wSourceAddr0 )? wResult_Last: wSourceDataRAM0;
+assign wSourceData1 = (wDestination_LastOperation == wSourceAddr1 )? wResult_Last: wSourceDataRAM1;
+
+RAM_DUAL_READ_PORT DataRam
+(
+	.Clock(         Clock        ),
+	.iWriteEnable(  rWriteEnable ),
+	.iReadAddress0( wInstruction[7:0] ),
+	.iReadAddress1( wInstruction[15:8] ),
+	.iWriteAddress( wDestination ),
+	
+	.iDataIn(       rResult      ),
+	.oDataOut0(     wSourceDataRAM0 ),
+	.oDataOut1(     wSourceDataRAM1 )
+);
+
+assign wIPInitialValue = (Reset) ? 8'b0 : wDestination;
+UPCOUNTER_POSEDGE IP
+(
+.Clock(   Clock                ), 
+.Reset(   Reset | rBranchTaken ),
+.Initial( wIPInitialValue + 1  ),
+.Enable(  1'b1                 ),
+.Q(       wIP_temp             )
+);
+assign wIP = (rBranchTaken) ? wIPInitialValue : wIP_temp;
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD1 
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[27:24]),
+	.Q(wOperation)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD2
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[7:0]),
+	.Q(wSourceAddr0)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD3
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[15:8]),
+	.Q(wSourceAddr1)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD4
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wInstruction[23:16]),
+	.Q(wDestination)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFDest 
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(wDestination),
+	.Q(wDestination_LastOperation)
+);
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 16 ) FFDRresul 
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable(1'b1),
+	.D(rResult),
+	.Q(wResult_Last)
+);
+
+
+
+
+reg rFFLedEN;
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FF_LEDS
+(
+	.Clock(Clock),
+	.Reset(Reset),
+	.Enable( rFFLedEN ),
+	.D( wSourceData1 ),
+	.Q( oLed    )
+);
+
+assign wImmediateValue = {wSourceAddr1,wSourceAddr0};
+
+
+
+always @ ( * )
+begin
+	case (wOperation)
+	//-------------------------------------
+	`NOP:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+	end
+	//-------------------------------------
+	`ADD:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rResult      <= wSourceData1 + wSourceData0;
+		//$display("%dns ADD R%d = %x + %x = %x!!!",$time, wDestination, wSourceData1, 
+		//		wSourceData1, 	rResult	);
+	end
+	//-------------------------------------
+	`SUB:
+	begin
+		rFFLedEN     <= 1'b0;
+		rBranchTaken <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rResult      <= wSourceData1 - wSourceData0;
+	end
+	//-------------------------------------
+	`STO:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b1;
+		rBranchTaken <= 1'b0;
+		rResult      <= wImmediateValue;
+	end
+	//-------------------------------------
+	`BLE:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		if (wSourceData1 <= wSourceData0 )
+			rBranchTaken <= 1'b1;
+		else
+			rBranchTaken <= 1'b0;
+		
+	end
+	//-------------------------------------	
+	`JMP:
+	begin
+		rFFLedEN     <= 1'b0;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b1;
+	end
+	//-------------------------------------	
+	`LED:
+	begin
+		rFFLedEN     <= 1'b1;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b0;
+	end
+	//-------------------------------------
+	default:
+	begin
+		rFFLedEN     <= 1'b1;
+		rWriteEnable <= 1'b0;
+		rResult      <= 0;
+		rBranchTaken <= 1'b0;
+	end	
+	//-------------------------------------	
+	endcase	
+end
+
+
+endmodule
